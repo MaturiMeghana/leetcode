144877d67ffb158abcf9c957b17b2f7079609757
MaturiMeghana <maturimeghana73@gmail.com>
2026-07-17 17:45:38 +0530

    Time: 28 ms (50%), Space: 12.4 MB (52.78%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.py
@@ -4,26 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        
-        pre={}
-        suff={}
         res=[]
-        for i in nums:
-            if i in suff:
-                suff[i]+=1
-            else:
-                suff[i]=1
-        for i in nums:
-            if i in pre:
-                pre[i]+=1
-            else:
-                pre[i]=1
-            suff[i]-=1
-            if suff[i]==0:
-                del suff[i]
-            res.append(len(pre)-len(suff))
+        for i in range(len(nums)):
+            left=len(set(nums[:i+1]))
+            right=len(set(nums[i+1:]))
+            res.append(left-right)
         return res
-            
+
+
+
+
         
 
         
